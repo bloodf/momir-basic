5ed8a94cfad71e1ad831968c971539f930395e35
Heitor Ramon Ribeiro <heitor.ramon@gmail.com>
2026-04-14 18:19:53 -0300

    chore(deps): switch thermal printer driver to 0.1.1
    Ultraworked with [Sisyphus](https://github.com/code-yeongyu/oh-my-openagent)
    
    Co-authored-by: Sisyphus <clio-agent@sisyphuslabs.ai>

--- metro.config.js
+++ metro.config.js
@@ -1,18 +1,2 @@
-const path = require('path');
-const { getDefaultConfig } = require('expo/metro-config');
-
-const projectRoot = __dirname;
-const localThermalPrinterPackage = path.resolve(projectRoot, '../expo-thermal-printer-driver');
-
-const config = getDefaultConfig(projectRoot);
-
-config.watchFolders = [...(config.watchFolders ?? []), localThermalPrinterPackage];
-config.resolver = {
-  ...(config.resolver ?? {}),
-  extraNodeModules: {
-    ...((config.resolver && config.resolver.extraNodeModules) || {}),
-    'react-native-thermal-printer-driver': localThermalPrinterPackage,
-  },
-};
-
-module.exports = config;
+const { getDefaultConfig } = require("expo/metro-config");
+module.exports = getDefaultConfig(__dirname);
